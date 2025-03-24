badc7a716b0621ba2586e9a7fe8ccbfdb1de46bb
Samuel Robinson <samueljzrobinson@gmail.com>
2025-03-24 16:34:05 +1000

    Basic Setup

---
+++ main.js
@@ -0,0 +1,52 @@
+class LinkedList{
+  constructor(){
+    this.head = null;
+  }
+
+  append(value){
+
+  }
+
+  prepend(value){
+
+  }
+
+  size(){
+    
+  }
+
+  head(){
+
+  }
+
+  tail(){
+
+  }
+
+  at(index){
+
+  }
+
+  pop(){
+
+  }
+
+  contains(value){
+
+  }
+
+  find(value){
+
+  }
+
+  toString(){
+
+  }
+}
+
+class Node{
+  constructor (){
+    this.value = null;
+    this.nextNode = null;
+  }
+}
